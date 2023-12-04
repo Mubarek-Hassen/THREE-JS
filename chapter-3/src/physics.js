f24871318eed902999b976ec47a883dee2bfedca
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-12-04 00:06:44 -0500

    contact material added

--- chapter-3/src/physics.js
+++ chapter-3/src/physics.js
@@ -48,11 +48,27 @@ const environmentMapTexture = cubeTextureLoader.load([
 const world = new CANNON.World()
 world.gravity.set(0, -9.82, 0)
 
+//MATERIAL
+const concreteMaterial = new CANNON.Material("concrete")
+const plasticMaterial = new CANNON.Material("plastic")
+
+
+const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+  concreteMaterial,
+  plasticMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.7
+  }
+)
+world.addContactMaterial(concretePlasticContactMaterial)
+
 //  To create a body, we need to create a shape
 const sphereShape = new CANNON.Sphere(0.5)
 const sphereBody = new CANNON.Body({
   mass: 1,
   position: new CANNON.Vec3(0, 3, 0),
+  material: plasticMaterial,
   shape: sphereShape,
 })
 
@@ -62,6 +78,7 @@ const floorShape = new CANNON.Plane()
 const floorBody = new CANNON.Body()
 floorBody.mass = 0
 floorBody.addShape(floorShape)
+floorBody.material = concreteMaterial
 floorBody.quaternion.setFromAxisAngle(
   new CANNON.Vec3(-1, 0, 0),
   Math.PI/2
